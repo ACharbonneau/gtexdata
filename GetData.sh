ccbacb4039a994c35ae4a101b39f1090fcd59d5e
Amanda Charbonneau <charbo24@msu.edu>
2020-04-21 17:04:15 -0400

    Update GetData.sh

--- GetData.sh
+++ GetData.sh
@@ -11,3 +11,5 @@ wget https://storage.googleapis.com/gtex_analysis_v8/annotations/GTEx_Analysis_v
 
 wget https://storage.googleapis.com/gtex_analysis_v8/annotations/GTEx_Analysis_v8_Annotations_SampleAttributesDD.xlsx
 
+## File info from downloading "Metadata" from here:
+# https://trace.ncbi.nlm.nih.gov/Traces/study/?acc=phs000424&o=biospecimen_repository_sample_id_sam_s%3Aa
