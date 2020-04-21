f1b22fb5a32b61b96a866d695414b7175a594b5a
Amanda Charbonneau <charbo24@msu.edu>
2020-04-21 17:03:19 -0400

    Create Munge.sh

---
+++ Munge.sh
@@ -0,0 +1,21 @@
+######################################
+# Per the C2M2 model level 0 data is: 
+# id_namespace - 
+# id - SAMPID
+# size_in_bytes - 
+# sha256 -
+# md5 - 
+# persistant_id - 
+# filename - SMGTC
+# 
+# Per the C2M2 model level 1 data adds: 
+# assay_type - 
+# host_species - Homo sapiens
+# anatomy - SMUBRID
+# project - 
+# file_format - 
+# data_type - 
+# subject -
+# sample - 
+#
+######################################
