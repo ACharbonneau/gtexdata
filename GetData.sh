7b0d1464a983a5403bcae613fe03361e8dcd5def
Amanda Charbonneau <charbo24@msu.edu>
2020-04-21 16:02:57 -0400

    Create GetData.sh

---
+++ GetData.sh
@@ -0,0 +1,13 @@
+## Get data files
+
+## Tissue variables
+wget https://storage.googleapis.com/gtex_analysis_v8/annotations/GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt
+
+## Patient variables
+
+wget https://storage.googleapis.com/gtex_analysis_v8/annotations/GTEx_Analysis_v8_Annotations_SubjectPhenotypesDS.txt
+
+## descriptions for Tissue variables
+
+wget https://storage.googleapis.com/gtex_analysis_v8/annotations/GTEx_Analysis_v8_Annotations_SampleAttributesDD.xlsx
+
